0d290e0991b1c79aaf35a31862e67fa18d3e2107
Ahmad Bin Rashid <144414852+Ahmad-157@users.noreply.github.com>
2023-09-24 18:59:31 +0500

    PFLab2

---
+++ PFLab 2/parallelogram.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout<<"            "<<endl;
+cout<<"       ____________   "<<endl;
+cout<<"      ;           ;  "<<endl;
+cout<<"     ;           ;   "<<endl;
+cout<<"    ;           ;    "<<endl;
+cout<<"   ;___________;     "<<endl;
+cout<<"                     "<<endl;
+
+}
